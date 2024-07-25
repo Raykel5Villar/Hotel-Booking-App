4d3f527699af33ac0757d38c2d28ed215b8e589b
Raykel5Villar <raykelvillar@gmail.com>
2024-07-24 21:39:20 -0400

    Adding Some futures

--- main.py
+++ main.py
@@ -1,75 +1,50 @@
-import requests
-import selectorlib
-import smtplib, ssl
-import os
-import time
-import sqlite3
+import pandas as pd
 
-"INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
-"SELECT * FROM events WHERE date='2088.10.15'"
-
-URL = "http://programmer100.pythonanywhere.com/tours/"
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
-connection = sqlite3.connect("data.db")
-
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+df = pd.read_csv("hotels.csv", dtype={"id": str})
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
+    def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+class ReversationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
-    username = "app8flask@gmail.com"
-    password = "qyciukmocfaiarse"
+    def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel Name: {self.hotel.name}
+        """
+        return content
 
-    receiver = "app8flask@gmail.com"
-    context = ssl.create_default_context()
+print(df)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReversationTicket(customer_name=name, hotel_object=hotel)
+    print(reservation_ticket.generate())
 
-
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
-
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
-
-
-if __name__ == "__main__":
-    while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
-        print(extracted)
-
-        if extracted != "No upcoming tours":
-            row = read(extracted)
-            if not row:
-                store(extracted)
-                send_email(message="Hey, new event was found!")
-        time.sleep(2)
+else:
+    print("Hotel is not free.")
